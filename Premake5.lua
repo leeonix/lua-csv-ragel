b3e7bdbeb764112e7765f812750b596ca4f66383
leeonix <real-like@yeah.net>
2015-07-17 08:58:03 +0800

    modify Premake5.lua to use ragel's rules

--- Premake5.lua
+++ Premake5.lua
@@ -1,13 +1,34 @@
 
+externalrule 'ragel'
+    location 'ragel'
+    filename 'ragel'
+    fileextension '.rl'
+    propertydefinition {
+        name = 'OutputFileName',
+    }
+    propertydefinition {
+        name = 'Outputs',
+    }
+    propertydefinition {
+        name = 'CodeStyle',
+    }
+
 solution 'csv'
     configurations { 'Debug', 'Release' }
     language 'C'
-    defines { 'WIN32', '_WINDOWS' }
+    defines { 'WIN32', '_WIN32', 'WINDOWS', '_WINDOWS' }
     flags { 'StaticRuntime', 'NoManifest', }
 
     location 'build'
     targetdir 'bin'
     objdir 'obj'
+
+    rules { 'ragel' }
+    ragelVars {
+        OutputFileName = '../src/%(Filename).c',
+        Outputs        = '%(OutputFileName)',
+        CodeStyle      = '6',
+    }
 
     filter { 'action:vs*' }
         defines { '_CRT_SECURE_NO_WARNINGS' }
@@ -24,42 +45,22 @@ solution 'csv'
 
 project 'csv'
     kind 'StaticLib'
-
     includedirs {
         'src',
     }
-
     files {
         'src/csv.h',
         'src/csv.rl',
     }
 
-    rules { 'ragel' }
-    externalrule 'ragel'
-        location 'ragel'
-        filename 'ragel'
-        fileextension '.rl'
-        propertydefinition {
-            name  = "OutputFileName",
-            kind  = "string",
---            value = 'src\\%(Filename).c',
-        }
-
---    filter { 'configurations:Release' }
---        ragelVars {
---            OutputFileName = 'src\\%(Filename).c',
---        }
-
 project 'test'
     kind 'ConsoleApp'
-
     includedirs {
         'src',
     }
     links {
         'csv',
     }
-
     files {
         'test/test.c',
     }
